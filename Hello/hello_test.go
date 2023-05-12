12f2c23fcb3ce7a824015b9ec631307aacd7702b
amit <amit.pythondeveloper@gmail.com>
2023-05-12 12:35:18 +0530

    Refactored tests

--- Hello/hello_test.go
+++ Hello/hello_test.go
@@ -2,24 +2,25 @@ package main
 
 import "testing"
 
+func AssertCorrectGreet(got string, want string, t *testing.T) {
+	if got != want {
+		t.Errorf("should print %q but got %q", want, got)
+	}
+}
+
 func TestHello(t *testing.T) {
 
 	t.Run("Saying hello to people when name supplied", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello Chris!"
+		AssertCorrectGreet(got, want, t)
 
-		if got != want {
-			t.Errorf("should print %q but got %q", want, got)
-		}
 	})
 
 	t.Run("Saying hello to all if name not supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello World!"
-
-		if got != want {
-			t.Errorf("should print %q but got %q", want, got)
-		}
+		AssertCorrectGreet(got, want, t)
 	})
 
 }
