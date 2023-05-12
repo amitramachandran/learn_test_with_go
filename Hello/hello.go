a96af880178bce2416e1e51e62a38ad3ee2ace2f
amit <amit.pythondeveloper@gmail.com>
2023-05-12 13:15:32 +0530

    made greeting prefix private and small refactor

--- Hello/hello.go
+++ Hello/hello.go
@@ -2,21 +2,23 @@ package main
 
 import "fmt"
 
-const english_prefix = "Hello "
-const greeting_postfix = "!"
-const french_prefix = "Bonjour "
-const spanish_prefix = "Hola "
+const (
+	english_prefix   = "Hello "
+	greeting_postfix = "!"
+	french_prefix    = "Bonjour "
+	spanish_prefix   = "Hola "
+)
 
 func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	greeting := GreetingPrefix(lang) + name + greeting_postfix
+	greeting := greetingPrefix(lang) + name + greeting_postfix
 	return greeting
 
 }
 
-func GreetingPrefix(lang string) string {
+func greetingPrefix(lang string) string {
 	var prefix string
 	switch lang {
 	case "french":
